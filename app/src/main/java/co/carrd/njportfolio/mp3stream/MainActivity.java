85640a75863c6590b01b3b40f6688bba105076ce
NicholasJohansan <nicholasjohansan@gmail.com>
2022-07-03 23:14:18 +0800

    Implement bottom navigation between main fragments

--- app/src/main/java/co/carrd/njportfolio/mp3stream/MainActivity.java
+++ app/src/main/java/co/carrd/njportfolio/mp3stream/MainActivity.java
@@ -1,8 +1,18 @@
 package co.carrd.njportfolio.mp3stream;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+import co.carrd.njportfolio.mp3stream.Fragments.EqualizerFragment;
+import co.carrd.njportfolio.mp3stream.Fragments.LibraryFragment;
+import co.carrd.njportfolio.mp3stream.Fragments.SearchFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +20,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnItemSelectedListener(navListener);
+
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.main_fragment_container, new LibraryFragment()).commit();
     }
+
+    private NavigationBarView.OnItemSelectedListener navListener = item -> {
+        Fragment selectedFragment = null;
+
+        switch (item.getItemId()) {
+            case R.id.bottom_nav_library:
+                selectedFragment = new LibraryFragment();
+                break;
+            case R.id.bottom_nav_search:
+                selectedFragment = new SearchFragment();
+                break;
+            case R.id.bottom_nav_equalizer:
+                selectedFragment = new EqualizerFragment();
+                break;
+        }
+
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.main_fragment_container, selectedFragment).commit();
+
+        return true;
+    };
 }
