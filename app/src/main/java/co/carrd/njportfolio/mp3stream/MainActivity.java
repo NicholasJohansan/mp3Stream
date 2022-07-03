7fb69fd37e4dcab39a5862023a6591e09ca0eadb
NicholasJohansan <nicholasjohansan@gmail.com>
2022-07-03 22:36:03 +0800

    Init

---
+++ app/src/main/java/co/carrd/njportfolio/mp3stream/MainActivity.java
@@ -0,0 +1,14 @@
+package co.carrd.njportfolio.mp3stream;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
