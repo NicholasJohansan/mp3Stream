82af5b81504057bf83cce245d16ca2081ae01795
NicholasJohansan <nicholasjohansan@gmail.com>
2022-08-02 20:44:12 +0800

    Add miniplayer swipe playout POC

--- app/src/main/java/co/carrd/njportfolio/mp3stream/MainActivity.java
+++ app/src/main/java/co/carrd/njportfolio/mp3stream/MainActivity.java
@@ -1,12 +1,22 @@
 package co.carrd.njportfolio.mp3stream;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.util.TypedValue;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
+import com.colorgreen.swiper.OnSwipeTouchListener;
+import com.colorgreen.swiper.SwipeAction;
+import com.colorgreen.swiper.SwipeActionListener;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
@@ -20,12 +30,64 @@ public class MainActivity extends AppCompatActivity {
     private Fragment searchFragment = SearchFragment.getInstance();
     private Fragment equalizerFragment = new EqualizerFragment();
 
+    private LinearLayout miniPlayer;
+    private RelativeLayout mainLayout;
+
+    private int convertToPixels(int dp) {
+        return Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, getResources().getDisplayMetrics()));
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+
+        float screenHeight = getResources().getDisplayMetrics().heightPixels;
+
+        mainLayout = findViewById(R.id.main_layout);
+        ConstraintLayout miniPlayerView = findViewById(R.id.mini_player_view);
+        float targetHeight = getResources().getDisplayMetrics().heightPixels - convertToPixels(48);
+        float startHeight = convertToPixels(0);
+        miniPlayer = findViewById(R.id.mini_player);
+        miniPlayer.setY(targetHeight);
+        OnSwipeTouchListener swipeTouchListener = new OnSwipeTouchListener();
+        SwipeAction swipeAction = new SwipeAction();
+        swipeAction.setDirection(SwipeAction.DragDirection.Up);
+        swipeAction.setSteps(new float[]{targetHeight - convertToPixels(64), startHeight});
+        swipeAction.setDragThreshold(0.2f);
+        swipeAction.setSwipeActionListener(new SwipeActionListener() {
+            @Override
+            public void onDragStart(float v, float v1) {
+
+            }
+
+            @Override
+            public void onDrag(float v, float v1) {
+                miniPlayer.setY(v);
+                bottomNav.setY(screenHeight - ((convertToPixels(48)) * (v/targetHeight)));
+                Log.d("DRAG_END_",  " " + v + " " + (double) (v/targetHeight) + " " + 1792.0/1792.0 + " " + 1792/1792);
+                bottomNav.setAlpha((float) (1.0 * (v/targetHeight)));
+            }
+
+            @Override
+            public void onDragEnd(float v, float v1) {
+                Log.d("DRAG_END", String.valueOf(v));
+                // 0.0 -> expanded
+                if (v == 0.0) {
+                    bottomNav.setAlpha(0);
+                    bottomNav.setY(screenHeight);
+                } else {
+                    bottomNav.setAlpha(1);
+                    bottomNav.setY(screenHeight - convertToPixels(48));
+                }
+            }
+        });
+        swipeTouchListener.addAction(swipeAction);
+        swipeTouchListener.attachToView(miniPlayerView);
+        swipeAction.collapse();
+
+
         bottomNav.setOnItemSelectedListener(navListener);
         bottomNav.setItemIconTintList(null); // This line is needed to have gradient icons work
 
