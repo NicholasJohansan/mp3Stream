362b3d4c3fd80fa32d57e854edbc2bc4cec83780
NicholasJohansan <nicholasjohansan@gmail.com>
2022-08-03 02:51:30 +0800

    Add base ArtistDetailsFragment

---
+++ app/src/main/java/co/carrd/njportfolio/mp3stream/Search/ArtistDetailsFragment.java
@@ -0,0 +1,22 @@
+package co.carrd.njportfolio.mp3stream.Search;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import co.carrd.njportfolio.mp3stream.R;
+
+public class ArtistDetailsFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View fragmentView = inflater.inflate(R.layout.fragment_artist_details, container, false);
+        return fragmentView;
+    }
+}
