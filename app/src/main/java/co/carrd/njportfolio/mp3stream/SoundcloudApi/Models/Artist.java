013b43b3a5b812dd329036edbff33f01e861f1ff
NicholasJohansan <nicholasjohansan@gmail.com>
2022-08-03 17:12:47 +0800

    Implement Parcelable on Artist

--- app/src/main/java/co/carrd/njportfolio/mp3stream/SoundcloudApi/Models/Artist.java
+++ app/src/main/java/co/carrd/njportfolio/mp3stream/SoundcloudApi/Models/Artist.java
@@ -1,8 +1,11 @@
 package co.carrd.njportfolio.mp3stream.SoundcloudApi.Models;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.net.URI;
 
-public class Artist {
+public class Artist implements Parcelable {
   private int id;
   private String name;
   private String bannerUrl;
@@ -32,4 +35,40 @@ public class Artist {
     return playlistCount;
   }
 
+  // Parcelable implementation
+  protected Artist(Parcel in) {
+    id = in.readInt();
+    name = in.readString();
+    bannerUrl = in.readString();
+    avatarUrl = in.readString();
+    songCount = in.readInt();
+    playlistCount = in.readInt();
+  }
+
+  public static final Creator<Artist> CREATOR = new Creator<Artist>() {
+    @Override
+    public Artist createFromParcel(Parcel in) {
+      return new Artist(in);
+    }
+
+    @Override
+    public Artist[] newArray(int size) {
+      return new Artist[size];
+    }
+  };
+
+  @Override
+  public int describeContents() {
+    return 0;
+  }
+
+  @Override
+  public void writeToParcel(Parcel parcel, int i) {
+    parcel.writeInt(id);
+    parcel.writeString(name);
+    parcel.writeString(bannerUrl);
+    parcel.writeString(avatarUrl);
+    parcel.writeInt(songCount);
+    parcel.writeInt(playlistCount);
+  }
 }
