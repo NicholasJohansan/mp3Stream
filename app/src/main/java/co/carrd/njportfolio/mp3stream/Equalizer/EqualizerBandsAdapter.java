20653aad0352de455fa985af0c95c6994c47f08c
NicholasJohansan <nicholasjohansan@gmail.com>
2022-08-05 18:23:36 +0800

    Add equalizer bands in xml

---
+++ app/src/main/java/co/carrd/njportfolio/mp3stream/Equalizer/EqualizerBandsAdapter.java
@@ -0,0 +1,69 @@
+package co.carrd.njportfolio.mp3stream.Equalizer;
+
+import android.content.Context;
+import android.media.audiofx.Equalizer;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.SeekBar;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.Arrays;
+
+import co.carrd.njportfolio.mp3stream.R;
+
+public class EqualizerBandsAdapter extends RecyclerView.Adapter<EqualizerBandsAdapter.EqualizerBandViewHolder> {
+    private Equalizer equalizer;
+
+    public EqualizerBandsAdapter(Equalizer equalizer) {
+        this.equalizer = equalizer;
+    }
+
+    @NonNull
+    @Override
+    public EqualizerBandViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View equalizerBandView = inflater.inflate(R.layout.item_equalizer_band, parent, false);
+        return new EqualizerBandViewHolder(equalizerBandView);
+    }
+
+    @Override
+    public int getItemCount() {
+        return equalizer.getNumberOfBands();
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull EqualizerBandViewHolder holder, int position) {
+        holder.bindBandData(position);
+    }
+
+    public class EqualizerBandViewHolder extends RecyclerView.ViewHolder {
+        private SeekBar equalizerBandBar;
+
+        public EqualizerBandViewHolder(@NonNull View itemView) {
+            super(itemView);
+            equalizerBandBar = itemView.findViewById(R.id.equalizer_band_bar);
+        }
+
+        public void bindBandData(int bandNumber) {
+            //
+            short band = equalizer.getBand(bandNumber);
+            short bandLevel = equalizer.getBandLevel((short) bandNumber);
+            int centerFreq = equalizer.getCenterFreq((short) bandNumber);
+            String freqRange = String.join(",", Arrays.stream(equalizer.getBandFreqRange((short) bandNumber)).mapToObj(String::valueOf).toArray(String[]::new));
+            short[] bandLevelRange = equalizer.getBandLevelRange();
+            String levelRange = "";
+            for (short s : bandLevelRange) {
+                levelRange += "," + s;
+            }
+            equalizerBandBar.setMin(bandLevelRange[0]);
+            equalizerBandBar.setMax(bandLevelRange[1]);
+            equalizerBandBar.setProgress(bandLevel);
+            Log.d("EQUALIZER", band + " " + bandLevel + " " + centerFreq + " " + freqRange + " " + levelRange);
+        }
+    }
+}
--- app/src/main/java/co/carrd/njportfolio/mp3stream/Equalizer/EqualizerFragment.java
+++ app/src/main/java/co/carrd/njportfolio/mp3stream/Equalizer/EqualizerFragment.java
@@ -1,18 +1,24 @@
 package co.carrd.njportfolio.mp3stream.Equalizer;
 
+import android.graphics.Rect;
 import android.media.MediaPlayer;
 import android.media.audiofx.Equalizer;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import co.carrd.njportfolio.mp3stream.MainActivity;
 import co.carrd.njportfolio.mp3stream.MainApplication;
@@ -20,25 +26,63 @@ import co.carrd.njportfolio.mp3stream.R;
 import co.carrd.njportfolio.mp3stream.Utils.UiUtils;
 
 public class EqualizerFragment extends Fragment {
+    private RecyclerView recyclerView;
+    private Equalizer equalizer;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View fragmentView = inflater.inflate(R.layout.fragment_equalizer, container, false);
         setUpStyles(fragmentView);
 
+        // Link UI
+        recyclerView = fragmentView.findViewById(R.id.equalizer_bands_recycler_view);
+
         AutoCompleteTextView presetTextView = fragmentView.findViewById(R.id.preset_text_field);
-        Equalizer equalizer = new Equalizer(0, MainApplication.getInstance().getPlayer().getAudioSessionId());
+        equalizer = new Equalizer(0, MainApplication.getInstance().getPlayer().getAudioSessionId());
         String[] presets = new String[equalizer.getNumberOfPresets()];
         for (short i = 0; i < presets.length; i++) {
             presets[i] = equalizer.getPresetName(i);
         }
         presetTextView.setAdapter(new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1, presets));
         presetTextView.setText(presets[0], false);
+//        presetTextView.setOnItemClickListener((adapterView, view, position, id) -> {
+//            // Select Preset
+//            equalizer.usePreset((short) position);
+//        });
+
+
+
+//        equalizer.usePreset();
 
         return fragmentView;
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        // Set up equalizer band recycler view
+        recyclerView.setAdapter(new EqualizerBandsAdapter(equalizer));
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext(), RecyclerView.HORIZONTAL, false));
+        recyclerView.addItemDecoration(new RecyclerView.ItemDecoration() {
+            @Override
+            public void getItemOffsets(@NonNull Rect outRect, @NonNull View view, @NonNull RecyclerView parent, @NonNull RecyclerView.State state) {
+                super.getItemOffsets(outRect, view, parent, state);
+                int totalWidth = parent.getMeasuredWidth();
+                int itemCount = state.getItemCount();
+                int spacing = (totalWidth - convertToPixels(32) * itemCount) / (itemCount + 1);
+                Log.d("EQUALIZER", totalWidth + " " + spacing + " " + view.getMeasuredWidth() + " " + view.getPaddingLeft());
+//                if (parent.getChildLayoutPosition(view) != itemCount - 1) {
+                outRect.left = spacing;
+//                }
+            }
+        });
+    }
+
     private void setUpStyles(View fragmentView) {
         UiUtils.setToolbarGradientTitle(fragmentView);
+    }
+
+    private int convertToPixels(int dp) {
+        return Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, getResources().getDisplayMetrics()));
     }
 }
